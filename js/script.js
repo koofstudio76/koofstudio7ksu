d072c9b5ef21414a1d6a5444e7bca1920aa70a97
koofstudio76 <145318086+koofstudio76@users.noreply.github.com>
2024-07-04 00:02:22 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,38 @@
+
+gsap.registerPlugin("ScrollTrigger");
+
+
+
+ 
+
+  gsap.to(".box1", {
+     scrollTrigger: {
+    trigger: "#three",
+    start: "top 35%",
+    end: "bottom 85%",
+    scrub:4,   
+     },
+   scale:1.7,
+     duration:2,
+     r:699,
+    });
+   gsap.to(".box2", {
+      scrollTrigger: {
+     trigger: ".box2",
+     start: "top 175%",
+     end: "bottom 95%",
+     scrub:4,   
+      },
+      r:759,
+   scaleY:1.1,
+      scaleX:0.68,
+      
+      duration:2,
+     });
+
+
+     gsap.to(".mask", {r:959,scale:3.6, duration:3,});
+ // 3D Scroll
+
+
+ 
